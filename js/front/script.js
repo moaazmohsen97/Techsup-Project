54c0371de7cc038a039ea5aeea6c33fbab43d51e
moaazmohsen97 <moaazmohsen97@gmail.com>
2023-11-08 02:03:35 +0200

    fixed

--- js/front/script.js
+++ js/front/script.js
@@ -49,6 +49,28 @@ Author: Sweet Themes
 				$('.number').css({'transform': 'translateX(-50%) scale(' + (1+(rangePercent/100)) + ')', 'left': rangePercent+'%'});
 			});
 		});
+
+
+// progress
+		$(".next").click(function(){
+			if($(".step-wrapper li:last-child").hasClass('completed')){
+			  alert("completed");
+			   return
+			}
+			  $(".step-wrapper li.active").addClass("completed").removeClass("active").next('li').addClass("active");  
+		  });
+		  
+		  $(".previous").click(function(){
+			if($(".step-wrapper li:first-child").hasClass('active')){
+			  alert("Already on first step");
+			   return
+			}
+			  $(".step-wrapper li.active").removeClass("active completed").prev('li').addClass("active").removeClass('completed');
+			if($(".step-wrapper li:last-child").hasClass('completed')){
+			  $(".step-wrapper li:last-child").removeClass('completed').addClass('active')
+			}
+		  });
+		  
 		// togglePreloaderLoaded
 		// function togglePreloaderLoaded() {
 		// 	const preloader = document.querySelector('.preloader');
